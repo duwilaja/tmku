08b92f1c14de7dc905ad097a5cd319833ab39203
duwilaja <apaaja.software@gmail.com>
2023-09-06 16:11:54 +0700

    1stkomit

---
+++ datasave.php
@@ -0,0 +1,141 @@
+<?php
+$redirect=false;
+
+include 'inc.common.php';
+include 'inc.session.php';
+include 'inc.db.php';
+
+$code='404';
+$ttl='Error';
+$msgs='Not found';
+
+$conn = connect();
+
+$mn=post('mn',$conn);
+
+if($mn=='register'){
+	$passwd=post('passwd');
+	$email=post('email');
+	$sql=sql_insert("users","name,email,company,phone",$conn,"passwd,lastupd","md5('$passwd'),now()");
+	$rs=exec_qry($conn,$sql);
+	if(db_error($conn)==''){
+		$code='200'; $ttl='Success'; $msgs='Data saved';
+	}else{
+		$code='201'; $ttl='Failed'; $msgs="Email $email sudah terdaftar";
+	}
+}
+
+if($mn=='chgpwd'){
+	$code='200';
+	$o=post('opass',$conn);
+	$n=post('npass',$conn);
+	$r=post('rpass',$conn);
+	if($n!=$r){
+		$code="220"; $ttl="Failed";
+		$msgs="Please re type an equal new password";
+	}
+	if($o==''||$n==''){
+		$code="210"; $ttl="Failed";
+		$msgs="Current password and new password can not blank";
+	}
+	if($code=='200'){
+		$tname=$s_ACCESS=='T'?"bltstok_stores":"xtm_users";
+		$sql=sql_update($tname,"","user='$s_ID' and passwd=md5('$o')",$conn,"passwd","md5('$n')");
+		$rs=exec_qry($conn,$sql);
+		if(affected_row($conn)>0){
+			$code='200'; $ttl='Success'; $msgs='Password changed';
+		}else{
+			$code='201'; $ttl='Failed'; $msgs="Invalid current password";
+		}
+	}
+}
+
+if($mn=='user'){
+	$passwd=post('fpasswd',$conn);
+	$fcols=$passwd==''?'':'passwd';
+	$fvals=$passwd==''?'':"md5('$passwd')";
+	$res=crud($conn,$fcols,$fvals);
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+}
+if($mn=='cust'){
+	$res=crud($conn);
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+}
+if($mn=='serv'){
+	$res=crud($conn);
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+}
+if($mn=='prob'){
+	$res=crud($conn);
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+}
+
+if($mn=='ticket'){
+	$f="lastupdate,updatedby"; $v="now(),'$s_ID'";
+	$s=post("status"); $n=post("lastnote");
+	if($s=='solved'){$f.=",solvedon";$v.=",now()";}
+	if($s=='closed'){$f.=",closedon";$v.=",now()";}
+	
+	$res=crud($conn,$f,$v);
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+	if($code=='200'){
+		//save history
+		$up=upload_file("fattc","attc/");
+		$attc=$up[0]&&$up[1]!=''?$up[1]:'';
+		$sql=sql_insert("xtm_notes","ticketno,lastnote,status",$conn,"updatedon,updatedby,attc","now(),'$s_ID','$attc'");
+		$rs=exec_qry($conn,$sql);
+		if(db_error($conn)!=""){ $msgs="Failed saving history";}
+	}
+}
+if($mn=='ticketx'){
+	$tn=date("YmdHis");
+	$cols="rowid,ticketno,createdby,lastupdate,updatedby";
+	$vals="'$tn','$tn','$s_ID',now(),'$s_ID'";
+	if($s_ACCESS=='U'){
+		$cols.=",calltime";
+		$vals.=",now()";
+	}
+	$res=crud($conn,$cols,$vals);
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+	if($code=='200'){
+		//save history
+		$up=upload_file("fattc","attc/");
+		$attc=$up[0]&&$up[1]!=''?$up[1]:'';
+		$dtl=post('detail');
+		$sql=sql_insert("xtm_notes","rowid",$conn,"ticketno,lastnote,status,updatedon,updatedby,attc","$tn,'$dtl','new',now(),'$s_ID','$attc'");
+		$rs=exec_qry($conn,$sql);
+		if(db_error($conn)!=""){ $msgs="Failed saving history".db_error($conn);}
+	}
+}
+
+if($mn=='menu'){
+	$up=upload_file("fpict","pict/");
+	$sv=post('sv');
+	$pict=$up[0]&&$up[1]!=''?$up[1]:post('pict');
+	if($sv=='NEW'){
+		$pict=$up[0]&&$up[1]!=''?$up[1]:'';
+	}
+	$res=crud($conn,"pict","'$pict'");
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+}
+if($mn=='content'){
+	$up=upload_file("fpict","pict/");
+	$pdf=upload_file("ffile","docs/");
+	$sv=post('sv');
+	$pict=$up[0]&&$up[1]!=''?$up[1]:post('pict');
+	if($sv=='NEW'){
+		$pict=$up[0]&&$up[1]!=''?$up[1]:'';
+	}
+	$file=$pdf[0]&&$pdf[1]!=''?$pdf[1]:post('file');
+	if($sv=='NEW'){
+		$file=$pdf[0]&&$pdf[1]!=''?$pdf[1]:'';
+	}
+	$res=crud($conn,"pict,file","'$pict','$file'");
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+}
+
+
+disconnect($conn);
+
+echo json_encode(array('code'=>$code,'ttl'=>$ttl,'msgs'=>$msgs));
+?>
---
+++ home.php
@@ -0,0 +1,192 @@
+<?php
+//$restrict_grp=array("S","A");
+
+include 'inc.common.php';
+include 'inc.session.php';
+include 'inc.head.php';
+include 'inc.menu.php';
+
+$ptitle="Dashboard";
+$breadcrumb="Home/$ptitle";
+$mn="home";
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    	<div class="content-header">
+		  <div class="container">
+			<div class="row mb-2">
+			  <div class="col-sm-6">
+				<h1 class="m-0 text-dark"> <?php echo $ptitle?></h1>
+			  </div><!-- /.col -->
+			  <div class="col-sm-6">
+				<?php echo breadcrumb($breadcrumb)?>
+			  </div><!-- /.col -->
+			</div><!-- /.row -->
+		  </div><!-- /.container-fluid -->
+		</div>
+	<div id="content">
+		<div class="container">
+			<div class="row">
+
+			  <div class="col-12 col-sm-6 col-md-3">
+				<div class="info-box">
+				  <span class="info-box-icon bg-info elevation-1"><i class="fas fa-sun"></i></span>
+
+				  <div class="info-box-content">
+					<span class="info-box-text">New</span>
+					<span class="info-box-number new-t">0</span>
+				  </div>
+				  <!-- /.info-box-content -->
+				</div>
+				<!-- /.info-box -->
+			  </div>
+			  <!-- /.col -->
+			  <div class="col-12 col-sm-6 col-md-3">
+				<div class="info-box mb-3">
+				  <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+
+				  <div class="info-box-content">
+					<span class="info-box-text">Solved</span>
+					<span class="info-box-number solved-t">0</span>
+				  </div>
+				  <!-- /.info-box-content -->
+				</div>
+				<!-- /.info-box -->
+			  </div>
+			  <!-- /.col -->
+
+			  <!-- fix for small devices only -->
+			  <div class="clearfix hidden-md-up"></div>
+
+			  <div class="col-12 col-sm-6 col-md-3">
+				<div class="info-box mb-3">
+				  <span class="info-box-icon bg-success elevation-1"><i class="fas fa-clock"></i></span>
+
+				  <div class="info-box-content">
+					<span class="info-box-text">Progress</span>
+					<span class="info-box-number progress-t">0</span>
+				  </div>
+				  <!-- /.info-box-content -->
+				</div>
+				<!-- /.info-box -->
+			  </div>
+			  <!-- /.col -->
+			  <div class="col-12 col-sm-6 col-md-3">
+				<div class="info-box mb-3">
+				  <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-hourglass-end"></i></span>
+
+				  <div class="info-box-content">
+					<span class="info-box-text">Pending</span>
+					<span class="info-box-number pending-t">0</span>
+				  </div>
+				  <!-- /.info-box-content -->
+				</div>
+				<!-- /.info-box -->
+			  </div>
+			  <!-- /.col -->
+			
+			</div>
+			<div class="row">
+				<div class="col-md-8">
+					<div class="card">
+					  <div class="card-header border-0">
+						<div class="d-flex justify-content-between">
+						  <h3 class="card-title">Task Created</h3>
+						</div>
+					  </div>
+					  <div class="card-body">
+						<div class="position-relative mb-4">
+						  <canvas id="daily-chart" height="250"></canvas>
+						</div>
+					  </div>
+					</div>
+					<!-- /.card -->
+				</div> 
+				<!-- /.col -->
+				<div class="col-md-4">
+					<div class="card">
+					  <div class="card-header border-0">
+						<div class="d-flex justify-content-between">
+						  <h3 class="card-title">Types</h3>
+						</div>
+					  </div>
+					  <div class="card-body">
+						<div class="position-relative mb-4">
+						  <canvas id="pie-chart" height="250"></canvas>
+						</div>
+					  </div>
+					</div>
+					<!-- /.card -->
+				</div> 
+				<!-- /.col -->
+			</div>
+			<!-- /.row -->
+
+		</div>
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+</div>
+<!-- ./wrapper -->
+
+<?php
+include 'inc.js.php';
+?>
+
+<script>
+var mypie,myday;
+$(document).ready(function(){
+	getwidget();
+	getpie();
+	getdaily();
+});
+function getwidget(){		
+	$.ajax({
+		type: 'POST',
+		url: 'data<?php echo $ext?>',
+		data: {q:'homewidget'},
+		success: function(data){
+				var json=JSON.parse(data);
+				for(var i=0;i<json['msgs'].length;i++){
+					  $('.'+json['msgs'][i]['label']+'-t').html(json['msgs'][i]['data']);
+				  }
+			},
+		error: function(xhr){
+				log(xhr);
+			}
+	});
+}
+function getpie(){	
+	$.ajax({
+		type: 'POST',
+		url: 'data<?php echo $ext?>',
+		data: {q:'homepie'},
+		success: function(data){
+				var json=JSON.parse(data);
+				mypie=pie('#pie-chart',json['msgs']);
+			},
+		error: function(xhr){
+				log(xhr);
+			}
+	});
+}
+function getdaily(){
+	$.ajax({
+		type: 'POST',
+		url: 'data<?php echo $ext?>',
+		data: {q:'homedaily'},
+		success: function(data){
+				var json=JSON.parse(data);
+				myday=series('#daily-chart','bar',['Total'],[],json['msgs']);
+			},
+		error: function(xhr){
+				log(xhr);
+			}
+	});
+}
+</script>
+
+</body>
+</html>
